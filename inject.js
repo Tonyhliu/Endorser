475a58085df67ce576a6d6138eb19bc4fdd1185e
Tony Hoyin Liu <tonyhoyinliu@gmail.com>
2017-01-16 12:51:53 -0800

    updated header color and unendorse btn

--- inject.js
+++ inject.js
@@ -3,24 +3,6 @@ var injected = injected || (function() {
   var methods = {};
 
   methods.getButtons = function() {
-    var buttons = {};
-    // var buttonArr = [];
-    // if (document.getElementsByClassName("endorse-minus")) {
-    //   buttonArr = document.getElementsByClassName("endorsing");
-    // }
-    //   if (buttonArr.length > 0) {
-    //     for (let i = 0; i < buttonArr.length; i++ ) {
-    //         buttonArr[i].click();
-    //     }
-    //   } else {
-    //     alert("No endorse buttons found!");
-    //   }
-    // };
-
-    // var buttonArr = document.getElementsByClassName('pv-skill-entity__endorse-actions');
-    // var unendorseArr = ($('[data-control-name="unendorse"]'));
-    // var endorseArr = ($('[data-control-name="endorse"]'));
-
     // if greater than 0, means its hidden
     if (document.querySelectorAll('.pv-skills-section__additional-skills[aria-expanded="false"]').length > 0) {
       // expands skills section
--- injectUnendorse.js
+++ injectUnendorse.js
@@ -2,17 +2,26 @@ var injectUnendorsed = injectUnendorsed || (function() {
   var methods = {};
 
   methods.getUnendorseButtons = function() {
-    var buttons = {};
-    var buttonArr = [];
-    buttonArr = document.getElementsByClassName("endorsed-by-viewer");
-
-    if (buttonArr.length > 0) {
-      while (!buttonArr.length >= 0) {
-        buttonArr[0].children[1].children[1].click();
+    // buttonArr = document.getElementsByClassName("endorsed-by-viewer");
+    if (document.querySelectorAll('.pv-skills-section__additional-skills[aria-expanded="false"]').length > 0) {
+      // expands skills section
+      document.getElementsByClassName('pv-skills-section__additional-skills')[0].click();
+    }
+    var unendorseArr = (document.querySelectorAll('[data-control-name="unendorse"]'));
+    if (unendorseArr.length > 0) {
+      for (let i = 0; i < unendorseArr.length; i++ ) {
+          unendorseArr[i].click();
       }
     } else {
       alert("No endorsements found!");
     }
+    // if (buttonArr.length > 0) {
+    //   while (!buttonArr.length >= 0) {
+    //     buttonArr[0].children[1].children[1].click();
+    //   }
+    // } else {
+    //   alert("No endorsements found!");
+    // }
   };
 
   chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
