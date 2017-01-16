6e169ad3e426e76991ac7ce5d04c789a1516d5b0
Tony Hoyin Liu <tonyhoyinliu@gmail.com>
2017-01-16 12:35:14 -0800

    updated endorse btn

--- inject.js
+++ inject.js
@@ -4,14 +4,32 @@ var injected = injected || (function() {
 
   methods.getButtons = function() {
     var buttons = {};
-    var buttonArr = [];
-    if (document.getElementsByClassName("endorse-minus")) {
-      buttonArr = document.getElementsByClassName("endorsing");
-    }
+    // var buttonArr = [];
+    // if (document.getElementsByClassName("endorse-minus")) {
+    //   buttonArr = document.getElementsByClassName("endorsing");
+    // }
+    //   if (buttonArr.length > 0) {
+    //     for (let i = 0; i < buttonArr.length; i++ ) {
+    //         buttonArr[i].click();
+    //     }
+    //   } else {
+    //     alert("No endorse buttons found!");
+    //   }
+    // };
 
-    if (buttonArr.length > 0) {
-      for (let i = 0; i < buttonArr.length; i++ ) {
-          buttonArr[i].click();
+    // var buttonArr = document.getElementsByClassName('pv-skill-entity__endorse-actions');
+    // var unendorseArr = ($('[data-control-name="unendorse"]'));
+    // var endorseArr = ($('[data-control-name="endorse"]'));
+
+    // if greater than 0, means its hidden
+    if (document.querySelectorAll('.pv-skills-section__additional-skills[aria-expanded="false"]').length > 0) {
+      // expands skills section
+      document.getElementsByClassName('pv-skills-section__additional-skills')[0].click();
+    }
+    var endorseArr = (document.querySelectorAll('[data-control-name="endorse"]'));
+    if (endorseArr.length > 0) {
+      for (let i = 0; i < endorseArr.length; i++ ) {
+          endorseArr[i].click();
       }
     } else {
       alert("No endorse buttons found!");
